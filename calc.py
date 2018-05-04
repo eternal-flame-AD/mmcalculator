a5da57c5bcfc725a6f2f813d43b8d0ec25315aa4
_eternal_flame <eternal_flame-AD@protonmail.com>
2018-05-04 22:51:37 +0800

    yapf

--- calc.py
+++ calc.py
@@ -1,38 +1,43 @@
-import sys;
+import sys
 
 # 定义优先级
-PRIORITY = "^,*,/,+,-";
+PRIORITY = "^,*,/,+,-"
 
-def main(expression): 
-	exp_list = list("".join(expression)) 
-	format_list = format(exp_list)
-	res = calculate(format_list);
-	print(res); #输出结果
+
+def main(expression):
+    exp_list = list("".join(expression))
+    format_list = format(exp_list)
+    res = calculate(format_list)
+    print(res)
+    #输出结果
+
 
 #按照优先级进行计算
 def priority_compute(format_list):
-	_format_list = format_list;
-	for oper in PRIORITY.split(","):
-		_format_list = compute(_format_list, oper);
-	return _format_list
+    _format_list = format_list
+    for oper in PRIORITY.split(","):
+        _format_list = compute(_format_list, oper)
+    return _format_list
+
 
 #格式化表达式
 def format(single_list):
-	temp_list = [];
-	temp_num = "";
-	for index, i in enumerate(single_list):
+    temp_list = []
+    temp_num = ""
+    for index, i in enumerate(single_list):
 
-		if ( isNumber(i) ): temp_num += i
+        if (isNumber(i)): temp_num += i
 
-		if ( isOperator(i) ):
-			if (temp_num != "") : temp_list.append(temp_num)
-			temp_list.append(i)
-			temp_num = ""
+        if (isOperator(i)):
+            if (temp_num != ""): temp_list.append(temp_num)
+            temp_list.append(i)
+            temp_num = ""
 
-		if ( len(single_list) - 1 == index ):
-			if (temp_num != "") : temp_list.append(temp_num)
+        if (len(single_list) - 1 == index):
+            if (temp_num != ""): temp_list.append(temp_num)
 
-	return temp_list;
+    return temp_list
+
 
 #计算带括号的表达式
 def calculate(format_list):
@@ -41,49 +46,53 @@ def calculate(format_list):
     if expression.find("(") == -1:
         return priority_compute(format_list)
 
-    parathesis = []#做符号栈检查括号匹配
+    parathesis = []  #做符号栈检查括号匹配
     for i in range(len(expression)):
-        if expression[i] == '(':#遇到左括号, 表达式起点
-            parathesis.append(i)#保存左括号的下标
-        elif expression[i] == ')':#遇到右括号, 表达式终点, 遇到完整的一段表达式则递归计算
+        if expression[i] == '(':  #遇到左括号, 表达式起点
+            parathesis.append(i)  #保存左括号的下标
+        elif expression[i] == ')':  #遇到右括号, 表达式终点, 遇到完整的一段表达式则递归计算
             if len(parathesis) != 0:
-                subexp = expression[parathesis[-1] : i + 1][1 : -1]#截取子表达式
-                if subexp.find("(") == -1:#如果子表达式不含有括号了则计算
-                    result = priority_compute(format(subexp))[0] #计算子表达式的结果
-                    pre = expression[0 : parathesis[-1]]#括号前面的表达式
-                    post = expression[i + 1 : ]#括号后面的表达式
-                    expression = pre + str(result) + post#拼接为新的表达式
-                    return calculate(expression)#递归计算
-                parathesis.pop()#删除一个左括号
+                subexp = expression[parathesis[-1]:i + 1][1:-1]  #截取子表达式
+                if subexp.find("(") == -1:  #如果子表达式不含有括号了则计算
+                    result = priority_compute(format(subexp))[0]  #计算子表达式的结果
+                    pre = expression[0:parathesis[-1]]  #括号前面的表达式
+                    post = expression[i + 1:]  #括号后面的表达式
+                    expression = pre + str(result) + post  #拼接为新的表达式
+                    return calculate(expression)  #递归计算
+                parathesis.pop()  #删除一个左括号
+
 
 #计算表达式
-def compute(format_list, operator): 
-	timer = 0
-	for index, i in enumerate(format_list):
-		if ( isOperator(i) and i == operator ):
-			timer+=1
-			left = float(format_list[index - 1])
-			right = float(format_list[index + 1])
-			if operator == "^" : format_list[index - 1] =  left ** right
-			if operator == "*" : format_list[index - 1] =  left * right
-			if operator == "/" : format_list[index - 1] =  left / right
-			if operator == "+" : format_list[index - 1] =  left + right
-			if operator == "-" : format_list[index - 1] =  left - right
-			del format_list[index];del format_list[index]
-	
-	if ( timer ): 
-		return compute(format_list, operator);
-	else:
-		return format_list;
-	
+def compute(format_list, operator):
+    timer = 0
+    for index, i in enumerate(format_list):
+        if (isOperator(i) and i == operator):
+            timer += 1
+            left = float(format_list[index - 1])
+            right = float(format_list[index + 1])
+            if operator == "^": format_list[index - 1] = left**right
+            if operator == "*": format_list[index - 1] = left * right
+            if operator == "/": format_list[index - 1] = left / right
+            if operator == "+": format_list[index - 1] = left + right
+            if operator == "-": format_list[index - 1] = left - right
+            del format_list[index]
+            del format_list[index]
+
+    if (timer):
+        return compute(format_list, operator)
+    else:
+        return format_list
+
 
 # 判断是否是运算符
-def isOperator(str): 
-	return True if str in PRIORITY.split(",") else False
+def isOperator(str):
+    return True if str in PRIORITY.split(",") else False
+
 
 # 判断是否是数字
 def isNumber(str):
-	return True if str.isdigit() or str == "." else False
+    return True if str.isdigit() or str == "." else False
 
-if __name__ == "__main__" :
-	main(sys.argv[1:])
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- mmcalc.py
+++ mmcalc.py
@@ -1,180 +1,202 @@
 import csv
 import calc
+
+
 class Equation:
-    def __init__(self,symbol,symbols,mass,name):
-        self.items=[(symbol,1)]    #should be like [('Mn',1),('Fe',2)]
-        self.symbols=symbols
-        self.mass=mass
-        self.name=name
-    
-    def parse_whole_chemical(self,string):
-        mult=""
-        pos=0
+    def __init__(self, symbol, symbols, mass, name):
+        self.items = [(symbol, 1)]  #should be like [('Mn',1),('Fe',2)]
+        self.symbols = symbols
+        self.mass = mass
+        self.name = name
+
+    def parse_whole_chemical(self, string):
+        mult = ""
+        pos = 0
         while isdigit(string[pos]):
-            mult+=string[pos]
-            pos+=1
-        if mult=="":
-            mult="1"
-        return (string[pos:],int(mult))
-    
+            mult += string[pos]
+            pos += 1
+        if mult == "":
+            mult = "1"
+        return (string[pos:], int(mult))
+
     def split_dots(self):
-        target=self.items[0]
-        self.items=[]
-        string=target[0].replace("·",'.')
+        target = self.items[0]
+        self.items = []
+        string = target[0].replace("·", '.')
         while True:
             try:
-                pos=string.index('.')
+                pos = string.index('.')
                 self.items.append(self.parse_whole_chemical(string[0:pos]))
-                string=string[pos+1:]
+                string = string[pos + 1:]
             except ValueError:
                 break
         self.items.append(self.parse_whole_chemical(string))
-    
+
     def split_paren(self):
-        items_new=[]
-        items_old=self.items
+        items_new = []
+        items_old = self.items
         for item in items_old:
-            string=item[0]
-            mult=item[1]
-            chemical_now=""
-            in_paren=False
-            pos=0
-            while pos<len(string):
-                char=string[pos]
-                if char=='(':
-                    assert in_paren==False,"'(' met, ')' expected"
-                    items_new.append((chemical_now,mult))
-                    chemical_now=""
-                    in_paren=True
-                elif char==")":
-                    assert in_paren==True,"')' met, '(' expected"
-                    digit=""
-                    while ((pos+1<len(string)) and (isdigit(string[pos+1]))):
-                        digit+=string[pos+1]
-                        pos+=1
-                    if digit=="":
-                        digit="1"
-                    items_new.append((chemical_now,mult*int(digit)))
-                    chemical_now=""
-                    in_paren=False
+            string = item[0]
+            mult = item[1]
+            chemical_now = ""
+            in_paren = False
+            pos = 0
+            while pos < len(string):
+                char = string[pos]
+                if char == '(':
+                    assert in_paren == False, "'(' met, ')' expected"
+                    items_new.append((chemical_now, mult))
+                    chemical_now = ""
+                    in_paren = True
+                elif char == ")":
+                    assert in_paren == True, "')' met, '(' expected"
+                    digit = ""
+                    while ((pos + 1 < len(string))
+                           and (isdigit(string[pos + 1]))):
+                        digit += string[pos + 1]
+                        pos += 1
+                    if digit == "":
+                        digit = "1"
+                    items_new.append((chemical_now, mult * int(digit)))
+                    chemical_now = ""
+                    in_paren = False
                 else:
-                    chemical_now+=char
-                pos+=1
-            if chemical_now!="":
-                items_new.append((chemical_now,mult))
-        self.items=items_new
-    
+                    chemical_now += char
+                pos += 1
+            if chemical_now != "":
+                items_new.append((chemical_now, mult))
+        self.items = items_new
+
     def split_symbols(self):
-        items_new=[]
-        items_old=self.items
+        items_new = []
+        items_old = self.items
         for item in items_old:
-            string=item[0]
-            mult=item[1]
-            pos=0
-            while pos<len(string):
-                if (pos+1<len(string)) and issymbol(string[pos:pos+2],self.symbols):
-                    this_symbol=string[pos:pos+2]
-                    pos+=2
-                    digit=""
-                    while (pos<len(string)) and isdigit(string[pos]):
-                        digit+=string[pos]
-                        pos+=1
-                    if digit=="":
-                        digit="1"
-                    items_new.append((this_symbol,mult*int(digit)))
-                elif issymbol(string[pos],self.symbols):
-                    this_symbol=string[pos]
-                    pos+=1
-                    digit=""
-                    while (pos<len(string)) and isdigit(string[pos]):
-                        digit+=string[pos]
-                        pos+=1
-                    if digit=="":
-                        digit="1"
-                    items_new.append((this_symbol,mult*int(digit)))
-                elif string[pos]==" ":
-                    pos+=1
+            string = item[0]
+            mult = item[1]
+            pos = 0
+            while pos < len(string):
+                if (pos + 1 < len(string)) and issymbol(
+                        string[pos:pos + 2], self.symbols):
+                    this_symbol = string[pos:pos + 2]
+                    pos += 2
+                    digit = ""
+                    while (pos < len(string)) and isdigit(string[pos]):
+                        digit += string[pos]
+                        pos += 1
+                    if digit == "":
+                        digit = "1"
+                    items_new.append((this_symbol, mult * int(digit)))
+                elif issymbol(string[pos], self.symbols):
+                    this_symbol = string[pos]
+                    pos += 1
+                    digit = ""
+                    while (pos < len(string)) and isdigit(string[pos]):
+                        digit += string[pos]
+                        pos += 1
+                    if digit == "":
+                        digit = "1"
+                    items_new.append((this_symbol, mult * int(digit)))
+                elif string[pos] == " ":
+                    pos += 1
                 else:
-                    raise ValueError("ERR:Unsplittable expression:",string)
-        self.items=items_new
-    
+                    raise ValueError("ERR:Unsplittable expression:", string)
+        self.items = items_new
+
     def dump(self):
         for item in self.items:
             print(item)
-    
+
     def calculate_mass(self):
-        sum=0
+        sum = 0
         for item in self.items:
-            this=getmass(item[0],self.mass)*item[1]
-            sum+=this
-            print('{0:<3}'.format(better_looking_symbol(item[0])),'{0:<10}'.format(getname(item[0],self.name)),'\t',getmass(item[0],self.mass),'g/mol\t * ',item[1],'\t = ',this,'\taccumulate:',sum)
-        print("==================================================================")
-        print("Total mass=",sum,"g/mol")
-    
+            this = getmass(item[0], self.mass) * item[1]
+            sum += this
+            print('{0:<3}'.format(better_looking_symbol(item[0])),
+                  '{0:<10}'.format(getname(item[0], self.name)), '\t',
+                  getmass(item[0], self.mass), 'g/mol\t * ', item[1], '\t = ',
+                  this, '\taccumulate:', sum)
+        print(
+            "=================================================================="
+        )
+        print("Total mass=", sum, "g/mol")
+
     def digest(self):
         self.split_dots()
         self.split_paren()
         self.split_symbols()
         self.calculate_mass()
 
+
 def better_looking_symbol(s):
-    if len(s)==1:
+    if len(s) == 1:
         return s[0].upper()
     else:
-        return s[0].upper()+s[1:].lower()
+        return s[0].upper() + s[1:].lower()
 
-def issymbol(string,symbols):
+
+def issymbol(string, symbols):
     for item in symbols:
-        if symbol_ident(string,item):
+        if symbol_ident(string, item):
             return True
     return False
 
+
 def isdigit(s):
-    return ((s>='0') and (s<='9'))
+    return ((s >= '0') and (s <= '9'))
 
-def getmass(s,mass):
+
+def getmass(s, mass):
     for item in mass:
-        if symbol_ident(item[0],s):
+        if symbol_ident(item[0], s):
             return item[1]
-    raise ValueError("ERR:Unknown symbol:",s)
+    raise ValueError("ERR:Unknown symbol:", s)
 
-def getname(s,name):
+
+def getname(s, name):
     for item in name:
-        if symbol_ident(item[0],s):
+        if symbol_ident(item[0], s):
             return item[1]
-    raise ValueError("ERR:Unknown symbol:",s)
+    raise ValueError("ERR:Unknown symbol:", s)
+
 
 def readcsv(fn):
-    readfile=open(fn)
+    readfile = open(fn)
     return csv.DictReader(readfile)
 
-def parse(equ,symbols,mass,name):
-    target=Equation(equ,symbols,mass,name)
+
+def parse(equ, symbols, mass, name):
+    target = Equation(equ, symbols, mass, name)
     target.digest()
 
+
 def init():
-    data=readcsv('data.csv')
-    symbols=[]
-    mass=[]
-    name=[]
+    data = readcsv('data.csv')
+    symbols = []
+    mass = []
+    name = []
     for row in data:
         symbols.append(row['symbol'])
-        mass.append((row['symbol'],float(row['mass'])))
-        name.append((row['symbol'],row['name']))
-    return symbols,mass,name
+        mass.append((row['symbol'], float(row['mass'])))
+        name.append((row['symbol'], row['name']))
+    return symbols, mass, name
 
-def symbol_ident(s1,s2):
-    return s1.upper()==s2.upper()
 
-def reader(symbols,mass,name):
+def symbol_ident(s1, s2):
+    return s1.upper() == s2.upper()
+
+
+def reader(symbols, mass, name):
     while True:
-        equ=input('Pls enter a chemical formula or expression, enter exit to stop:')
+        equ = input(
+            'Pls enter a chemical formula or expression, enter exit to stop:')
         if 'exit' in equ:
             break
-        elif ("*" in equ) or ('/' in equ) or ('+' in equ) or ('-' in equ) or ('^' in equ):
+        elif ("*" in equ) or ('/' in equ) or ('+' in equ) or ('-' in equ) or (
+                '^' in equ):
             calc.main(equ)
         else:
-            parse(equ,symbols,mass,name)
+            parse(equ, symbols, mass, name)
 
-symbols,mass,name=init()
-reader(symbols,mass,name)
+
+symbols, mass, name = init()
+reader(symbols, mass, name)
